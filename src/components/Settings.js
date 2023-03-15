eedd4d1bfe7cfa7c157fdee8f14ad488257db667
syed ahad <110287582+ahadsyedsh@users.noreply.github.com>
2023-03-16 03:10:19 +0530

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,11 +6,13 @@ export const Settings = () => {
 
     // to be implemented in context
     const { changeGreeting } = useContext(UserContext)
-
+    const handleChange = (e)=>{
+        changeGreeting(e.target.value)
+    }
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} />
+            <input type={'text'} onChange={handleChange}/>
         </div>
     )
 }
